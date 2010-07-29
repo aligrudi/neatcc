83c48d1142a6c780d105e572e5ae9737ad816b79
Ali Gholami Rudi <ali@rudi.ir>
2010-07-29 08:50:20 +0430

    cpp: include stddef.h for using offsetof()

--- cpp.c
+++ cpp.c
@@ -1,7 +1,8 @@
 #include <ctype.h>
 #include <fcntl.h>
-#include <stdlib.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
