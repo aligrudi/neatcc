116711517f7b80e0de2e03ecdd2468afca47e14d
Ali Gholami Rudi <ali@rudi.ir>
2010-12-11 22:37:31 +0330

    cpp: define __i386__; __x86_64__ is no longer supported

--- cpp.c
+++ cpp.c
@@ -134,7 +134,7 @@ static int include_file(char *path)
 int cpp_init(char *path)
 {
 	cpp_define("__STDC__", "");
-	cpp_define("__x86_64__", "");
+	cpp_define("__i386__", "");
 	cpp_define("__linux__", "");
 	return include_file(path);
 }
