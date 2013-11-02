3ad09ecb070e10bcd798ed948c5a8b7ddfb50c0a
Ali Gholami Rudi <ali@rudi.ir>
2013-11-02 22:20:14 +0330

    ncc.h: increase array limits for inexpensive resources

--- ncc.h
+++ ncc.h
@@ -1,20 +1,20 @@
 /* predefined array limits; (p.f. means per function) */
 #define SECLEN		(1 << 19)	/* size of CS section */
-#define NDATS		1024		/* number of DS data symbols */
+#define NDATS		4096		/* number of DS data symbols */
 #define NSYMS		4096		/* number of elf symbols */
-#define NREL		4096		/* number of elf relocations */
-#define NGLOBALS	1024		/* number of global variables */
+#define NREL		8192		/* number of elf relocations */
+#define NGLOBALS	2048		/* number of global variables */
 #define NLOCALS		1024		/* number of locals p.f. */
 #define NARGS		32		/* number of function/macro arguments */
 #define NTMPS		64		/* number of expression temporaries */
 #define NNUMS		1024		/* number of integer constants p.f. (arm.c) */
 #define NJMPS		4096		/* number of jmp instructions p.f. */
 #define NFUNCS		1024		/* number of functions */
-#define NENUMS		1024		/* number of enum constants */
+#define NENUMS		4096		/* number of enum constants */
 #define NTYPEDEFS	1024		/* number of typedefs */
 #define NSTRUCTS	512		/* number of structs */
 #define NFIELDS		128		/* number of fields in structs */
-#define NARRAYS		1024		/* number of arrays */
+#define NARRAYS		8192		/* number of arrays */
 #define NLABELS		1024		/* number of labels p.f. */
 #define NAMELEN		128		/* size of identifiers */
 #define NDEFS		1024		/* number of macros */
